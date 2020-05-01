06813efa5f237b23e64fb2e929e52005ddcd9489
Xenonstaforums <xenonstaforums@gmail.com>
2020-05-01 17:37:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name = "remocolab.py",
     version = "0.1",
     py_modules = ['remocolab'],
-    url = "https://github.com/demotomohiro/remocolab",
+    url = "https://github.com/sudo-ken/remocolab",
     author = "demotomohiro",
 )
